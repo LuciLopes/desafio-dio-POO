54a2a50286b6cafcf8d4a88ac703e6b72d5cad6e
Lucineia Muniz <92742583+LuciLopes@users.noreply.github.com>
2022-07-27 14:11:24 +0000

    alteração

--- br.com.dio.desafio/Dev.java
+++ br.com.dio.desafio/Dev.java
@@ -1,46 +1,63 @@
 package br.com.dio.desafio.dominio;
 
 import java.util.LinkedHashSet;
+import java.util.Objects;
 import java.util.Set;
 
 public class Dev {
-  private String nome;
-  private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
-  private Set<Conteudo> conteudosConcluidos = new LnkedHashSet<>();
+    private String nome;
+    private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudosConcluidos = new LnkedHashSet<>();
 
-  public void inscreverBootcamp(Bootcamp bootcamp) {}
+    public void inscreverBootcamp(Bootcamp bootcamp) {
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().add(this);
+    }
 
-  public void progredir() {}
+    public void progredir() {
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if(conteudo.isPresent()) {
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        } else {
+            System.err.println("Você não está matriculado em nenhum conteúdo!");
+        }
+    }
 
-  public void calcularTotalXp() {}
+    public double calcularTotalXp() {
+        return this.conteudosConluidos
+                 .stream()
+                 .mapToDouble(Conteudo::calcularXp)
+                 .sum();
+    }
 
-  public String getNome() {
-    return nome;
-  }
+    public String getNome() {
+        return nome;
+    }
 
-  public void setNome(String nome) {
-    this.nome = nome;
-  }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-  public Set<Conteudo> getConteudosInscritos() {
-    return conteudosInscritos;
-  }
+    public Set<Conteudo> getConteudosInscritos() {
+        return conteudosInscritos;
+    }
 
-  public void setConteudosInscritos(Set<Conteudo> conteudosInscritos) {
-    this.conteudosInscritos = conteudosInscritos;
-  }
+    public void setConteudosInscritos(Set<Conteudo> conteudosInscritos) {
+        this.conteudosInscritos = conteudosInscritos;
+    }
 
-  public Set<Conteudo> getConteudosConcluidos() {
-    return conteudosConcluidos;
-  }
+    public Set<Conteudo> getConteudosConcluidos() {
+        return conteudosConcluidos;
+    }
 
-  public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
-    this.conteudosConcluidos = conteudosConcluidos;
-  }
+    public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
+        this.conteudosConcluidos = conteudosConcluidos;
+    }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(nome, conteudosInscritos, conteudosConcluidos);
-  }
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, conteudosInscritos, conteudosConcluidos);
+    }
   
 }
