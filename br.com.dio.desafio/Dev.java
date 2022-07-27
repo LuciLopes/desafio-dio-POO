d667d44dab90848998124e4dbd94acd6fd3e6f3b
Lucineia Muniz <92742583+LuciLopes@users.noreply.github.com>
2022-07-27 14:34:25 +0000

    Dev.java

--- br.com.dio.desafio/Dev.java
+++ br.com.dio.desafio/Dev.java
@@ -1,8 +1,6 @@
 package br.com.dio.desafio.dominio;
 
-import java.util.LinkedHashSet;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 public class Dev {
     private String nome;
@@ -25,10 +23,19 @@ public class Dev {
     }
 
     public double calcularTotalXp() {
+        Iterator<Conteudo> iterator = this.conteudosConcluidos.iterator();
+        double soma = 0;
+        while(iterator.hasNext()){
+            double next = iterator.next().calcularXp();
+            soma += next;
+        }
+        return soma;
+
+    /*public double calcularTotalXp() {
         return this.conteudosConluidos
                  .stream()
                  .mapToDouble(Conteudo::calcularXp)
-                 .sum();
+                 .sum();*/
     }
 
     public String getNome() {
@@ -53,6 +60,14 @@ public class Dev {
 
     public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
         this.conteudosConcluidos = conteudosConcluidos;
+    }
+
+     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dev dev = (Dev) o;
+        return Objects.equals(nome, dev.nome) && Objects.equals(conteudosInscritos, dev.conteudosInscritos) && Objects.equals(conteudosConcluidos, dev.conteudosConcluidos);
     }
 
     @Override
